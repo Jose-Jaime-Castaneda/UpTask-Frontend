64fa744ae3650f9e376e55bbc2b6dc8f9e89b2eb
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-01 09:39:57 -0600

    Fix: Responsive Desing on Project Team View

--- src/views/projects/ProjectTeamView.tsx
+++ src/views/projects/ProjectTeamView.tsx
@@ -38,7 +38,7 @@ export default function ProjectTeamView() {
             <h1 className="text-5xl font-black">Administra los Colaboradores</h1>
             <p className="text-2xl font-light text-gray-500 mt-5">Revisa quien tiene acceso a tu proyecto</p>
 
-            <nav className="my-5 flex gap-3">
+            <nav className="my-5 flex flex-wrap gap-3">
                 <button
                     type="button"
                     className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
