ccd4f9dcc448424c3e2dff77909a0b73caf7be27
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-08-01 09:27:27 -0600

    Fix: Responsice Desing on Login

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -5,8 +5,8 @@ import { ToastContainer } from "react-toastify";
 export default function AuthLayout() {
     return (
         <>
-            <div className="bg-gray-800 min-h-screen">
-                <div className="py-10 lg:py-20 mx-auto">
+            <div className="bg-gray-800 min-h-screen px-5">
+                <div className="py-10 lg:py-20 mx-auto w-full sm:w-2/3 md:w-2/3 lg:w-1/3">
                     <Logo />
                     <div className="mt-10">
                         <Outlet />
